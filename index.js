8627561da76d1e9d66936654fb721a978c208558
Nico <nicopenaloza@gmail.com>
2021-04-16 15:44:45 -0300

    04-16-2021

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const config = require('./config.json');
 const { Telegraf } = require('telegraf');
+const mySql = require('mysql');
 
+const db_pb_bot = mySql.createConnection(config.database);
+
+
+
+
+const pin = require('./modules/pinController.js');
 
 const bot = new Telegraf(config.token);
 
+
 // Bot events
 bot.start((ctx) => { // Cuando el usuario escribe /start en el chat se ejecuta este mensaje.
+    
 
-    ctx.reply('Hola, Soy PaxaBot, Un robot que te va a enviar notificaciones acerca de PaxaPos');
+    //db_pb_bot.query(); // buscar el usuario en clients y añadirlo si no esta.
+
+    ctx.reply(`Hola ${ctx.message.from.first_name}, vos ya estas registrado para recibir nuestras notificaciones, si lo que queres es añadir un comercio para recibir sus notificaciones tambien, usa el comando "/pin"`); 
+       
     console.log('BOT:', ctx.message.from);
 
+});
+
+
+bot.on('message', (ctx) => {
+    ctx.reply(`Hola ${ctx.message.from.first_name} yo te voy a notificar lo que ocurra en tu comercio, para asignar un comercio a tu cuenta usa /pin.`);
+});
+
+
+bot.command('pin', (ctx) => {
+    //db_pb_bot.query(); // Buscar PIN en pb_pin_sites agarrat el site_id y unto con el telegram_id guardar en pb_clients_sites
 });
 
 console.log('Bot iniciado');
@@ -24,18 +46,17 @@ bot.launch();
 
 module.exports = {
 
-    SendMessage: function (id, text){
-
-        if (id.toString().length == 10){
-
-            bot.telegram.sendMessage(id, text);
-
-        } else {
-
-            console.log(`Verifique de haber escrito bien el ID: ${id}`);
-
-        }
+    SendMessage: function (data) {
         
+        if (typeof(data.to) == "number"){
+            data.to = [data.to];
+        }
+
+        data.to.forEach(dest => {
+            bot.telegram.sendMessage(dest, data.text);
+            console.log("envio mensaje");
+        });
+
     }
 
 }
